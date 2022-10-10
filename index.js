575a6e98aae132de8648ec7de54345a47cb700eb
Benxidosz <szabo.bence.sandor@gmail.com>
2022-10-11 01:53:11 +0200

    Second task done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const server = app.listen(3000, function () {
+const server = app.listen(3000, () => {
+    console.log("Server is listening on :3000")
 });
 app.use(express.static('static'));
 
